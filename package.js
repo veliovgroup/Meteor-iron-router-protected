eeb762973a546d3ac6a66c6525831c2705096045
Dmitriy A. Golev <ceo@veliov.com>
2016-04-18 19:34:37 +0300

    v1.1.5
     - Compatibility for Meteor > 1.3.*
     - Dependencies update

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:iron-router-protected',
-  version: '1.1.4',
+  version: '1.1.5',
   summary: 'Create protected and meteor-roles restricted routes within iron-router',
   git: 'https://github.com/VeliovGroup/Meteor-iron-router-protected',
   documentation: 'README.md'
@@ -8,7 +8,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use(['coffeescript', 'ostrio:iron-router-helper-class@1.0.0'], 'client');
+  api.use(['coffeescript', 'ostrio:iron-router-helper-class@1.0.1'], 'client');
   api.addFiles('iron-router-protected.coffee', 'client');
-  api.imply('ostrio:iron-router-helper-class@1.0.0');
 });
